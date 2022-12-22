2e2ab1a0a79773ca9d66929f0fca51925c669d4d
calcado <pecalcado@gmail.com>
2022-12-21 21:16:52 -0300

    pratica

---
+++ queries.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE curso;
+
+CREATE TABLE students(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE classes(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    studentId INTEGER NOT NULL FOREIGN KEY REFERENCES "students"("id"),
+    arrival DATE NOT NULL DEFAULT NOW(),
+    departure DATE NOT NULL
+
+);
+
+CREATE TABLE projects(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    studentId INTEGER NOT NULL FOREIGN KEY REFERENCES "students"("id"),
+    classId INTEGER NOT NULL FOREIGN KEY REFERENCES "classes"("id"),
+    deadline DATE NOT NULL,
+    categoryId INTEGER NOT NULL FOREIGN KEY REFERENCES "categories"("id")
+);
+
+CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
