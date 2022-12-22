0ea62f5e7f49f49e47c43133d80f05905c765d97
calcado <pecalcado@gmail.com>
2022-12-22 19:34:48 -0300

    correção final

--- queries.sql
+++ queries.sql
@@ -1,31 +1,45 @@
 CREATE DATABASE curso;
 
 CREATE TABLE students(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE classes(
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    studentId INTEGER NOT NULL REFERENCES "students"("id"),
-    arrival DATE NOT NULL DEFAULT NOW(),
-    departure DATE NOT NULL
+    name TEXT NOT NULL UNIQUE
+   
+);
+CREATE TABLE modules (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
 
+
+CREATE TABLE grades(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+CREATE TABLE classesStudents(
+    id SERIAL PRIMARY KEY,
+    "studentId" INTEGER NOT NULL REFERENCES "students"("id")
+    classId INTEGER NOT NULL REFERENCES "classes"("id")
+    "startDate" DATE NOT NULL,
+    "endDate" DATE NOT NULL
 );
 
 CREATE TABLE projects(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    studentId INTEGER NOT NULL REFERENCES "students"("id"),
-    classId INTEGER NOT NULL REFERENCES "classes"("id"),
-    deadline DATE NOT NULL,
-    categoryId INTEGER NOT NULL REFERENCES "categories"("id")
+    "moduleId" INTEGER NOT NULL REFERENCES "modules"("id")
 );
 
-CREATE TABLE categories(
+CREATE TABLE projectsStudents(
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+    "studentId" INTEGER NOT NULL REFERENCES "students"("id"),
+    "projectId" INTEGER NOT NULL REFERENCES "projects"("id"),
+    "date" DATE NOT NULL DEFAULT ,
+    "gradeId" INTEGER NOT NULL REFERENCES "grades"("id")
 );
